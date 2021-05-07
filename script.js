1ad816f4dce2f8c393bf407786ce9e7f5a1c98d7
Kacper Swietlaga <kacper.swietlaga@elcaro.pl>
2021-05-07 16:11:02 +0200

    add math operations

--- script.js
+++ script.js
@@ -53,3 +53,75 @@ console.log('someObject', typeof someObject);
 //         null,
 //         undefined,
 //         NaN.
+
+// Variables are case sensitive!
+//     result = 10
+// RESULT = 11
+// Lines above are different variables!
+
+// COMMENTS
+// One line:
+// comment
+
+//     Multi-line:
+/*
+comment
+comment
+comment
+*/
+
+// Basic math operations
+// + and -
+
+var a = 1 + 1;
+var b = 2 - 3;
+var c = a + b;
+
+// WARNING!
+//     + is also concatenation operator!
+//     Try:
+
+var result = 'Ala ' + 'ma' + 'kota';
+// It can make problems!
+
+// * and /
+var a = 2 / 2;
+var b = a * 2;
+var c = a * b;
+
+// Modulo %
+
+var a = 2 % 2; // result is 0
+var b = 4 % 3; // result is 1
+var c = 2 % 4; // result is 2
+
+var a = 2;
+var b = 4;
+a += a; // result is 4
+b -= b; // result is 0
+
+var a = 2
+var b = 4
+// Checkout on console:
+
+console.log('a--', a--);
+
+
+console.log('a', a);
+
+
+console.log('--a', --a);
+
+
+console.log('a', a);
+
+
+console.log('b++', b++);
+
+
+console.log('b', b);
+
+
+console.log('++b', ++b);
+
+console.log('b', b);
