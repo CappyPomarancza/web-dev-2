64983402dda9b41ec0d17f8b5b90ab2dfd652df1
Kacper Swietlaga <kacper.swietlaga@elcaro.pl>
2021-05-07 15:56:00 +0200

    add initial files, added couple variables and their types in script

---
+++ script.js
@@ -0,0 +1,55 @@
+// Data types in JavaScript
+
+// Primitive :
+//     undefined
+//     null
+//     number
+//     string
+
+// All other values are OBJECTS, including arrays and functions !
+
+// Null literal
+// null
+// Boolean literal
+// true
+// false
+// Numeric literals
+// 1234567890
+// 42
+// Null literal
+let fuel = null;
+console.log('fuel', typeof fuel);
+
+// Boolean literal
+let onlyTrue = true;
+let onlyFalse = false;
+console.log('onlyTrue', typeof onlyTrue);
+console.log('onlyFalse', typeof onlyFalse);
+
+// String literals:
+const name = 'Kacper';
+const lastName = "Swietlaga";
+console.log('name', typeof name);
+
+// Numeric literals
+const age = 28;
+const year = 2021;
+console.log('age', typeof age);
+
+// Array literal :
+const years = [1954, 1974, 1990, 2014];
+console.log('years', typeof years);
+
+// Object literals :
+const someObject = { a: 'foo', b: 'bar', c: 42 };
+console.log('someObject', typeof someObject);
+
+// All values are truthy unless they are defined as falsy.!!!!
+
+// Falsy variables are:
+//         false,
+//         0,
+//         "",
+//         null,
+//         undefined,
+//         NaN.
